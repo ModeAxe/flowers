48efecd985d738b7ee091aa3923bbe297ef28648
Elijah Temwani Zulu <modeaxe@Gmail.com>
2023-03-31 16:42:15 -0400

    fix spawn rotation + destroy on invisible

--- Assets/TapSpawn.cs
+++ Assets/TapSpawn.cs
@@ -6,6 +6,7 @@ public class TapSpawn : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject flower;
+    public float offset = 10f;
     void Start()
     {
         
@@ -14,17 +15,17 @@ public class TapSpawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
             Vector2 mousePosition = Input.mousePosition;
             Debug.Log(mousePosition);
-            Vector3 position = Camera.main.ScreenToWorldPoint(mousePosition);
+            Vector3 position = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, offset));
             Spawn(position);
         }
     }
 
     void Spawn(Vector3 position)
     {
-        Instantiate(flower, position, Quaternion.identity);
+        Instantiate(flower, position, ((Quaternion.LookRotation(-Camera.main.transform.forward) * Quaternion.Euler(90,0,0))));
     }
 }
