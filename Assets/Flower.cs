6d607aad2a325766ec491f7bac71c26ea76730e9
Elijah Temwani Zulu <modeaxe@Gmail.com>
2023-04-25 10:31:32 -0400

    flower random material on spawn

--- Assets/Flower.cs
+++ Assets/Flower.cs
@@ -12,14 +12,22 @@ public class Flower : MonoBehaviour
 
     public float lifetime;
 
+    public List<Material> flowerMaterials = new List<Material>();
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
 
         Vector3 torque = new Vector3(0,0,Random.Range(-maxRotation, maxRotation));
         rb.AddTorque(torque, ForceMode.Impulse);
-        transform.localScale = new Vector3(Random.Range(minScale, maxScale), Random.Range(minScale, maxScale), Random.Range(minScale, maxScale));
-        
+        var scaleValue = Random.Range(minScale, maxScale);
+        transform.localScale = new Vector3(scaleValue, scaleValue, scaleValue);
+
+        int material_index = (int) Random.Range(0, flowerMaterials.Count);
+
+        Debug.Log(material_index);
+
+        gameObject.GetComponent<Renderer>().material = flowerMaterials[material_index]; 
     }
 
     void Update()
